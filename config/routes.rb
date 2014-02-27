9e7a941047323cf92430f00b7764c46008595a63
Jeff Cohen <jeff@starterleague.com>
2014-02-26 22:09:33 -0600

    Ready for class

---
+++ config/routes.rb
@@ -0,0 +1,30 @@
+Week6::Application.routes.draw do
+
+  get "/" => 'products#index'
+
+  # Product-related URLs
+
+  get "/users/new" => 'users#new'
+  get "/users/create" => 'users#create'
+
+  # CREATE
+  get "/products/new" => "products#new"
+  get "/products/create" => "products#create"
+
+  # READ
+  get "/products" => 'products#index'
+  get "/products/:product_id/show" => "products#show"
+
+  # UPDATE
+  get "/products/:product_id/edit" => "products#edit"
+  get "/products/:product_id/update" => "products#update"
+
+  # DELETE
+  get "/products/:product_id/delete" => "products#destroy"
+
+
+  # Review-related URLs
+
+  get "/reviews/create" => 'reviews#create'
+
+end
