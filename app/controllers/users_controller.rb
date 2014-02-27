791af7746ab2c2bd05bd601bd63a3e8845e196d1
Jeff Cohen <jeff@starterleague.com>
2014-02-27 16:21:50 -0600

    Example user authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find_by(:id => params[:user_id])
+    if @user.id != session[:user_id]
+      redirect_to root_url, notice: "Nice try"
+    end
+  end
 end
